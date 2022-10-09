cacd679c880e2066b1543379de32aea6d71f92da
evan <416523707@qq.com>
2022-10-09 20:34:37 +0800

    abi

---
+++ src/abis/p0xeidon.ts
@@ -0,0 +1,9 @@
+export const ABI = [
+  { anonymous: false, inputs: [], name: "Long", type: "event" },
+  { anonymous: false, inputs: [], name: "Short", type: "event" },
+  { anonymous: false, inputs: [], name: "Swap", type: "event" },
+  { inputs: [], name: "long", outputs: [], stateMutability: "payable", type: "function" },
+  { inputs: [], name: "short", outputs: [], stateMutability: "payable", type: "function" },
+  { inputs: [], name: "swap", outputs: [], stateMutability: "payable", type: "function" },
+  { stateMutability: "payable", type: "receive" },
+];
